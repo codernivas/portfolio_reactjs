9bde917d78c81b5ea83f2c9e8b6a35dac46e6008
Nivas BN <nivas.bn@emphaticsense.com>
2025-07-28 09:36:47 +0530

    education

---
+++ src/styled.d.ts
@@ -0,0 +1,9 @@
+// src/styled.d.ts
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    bg: string;
+    // Add any other theme properties you're using
+  }
+}
